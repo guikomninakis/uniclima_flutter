a7ce5b02e04e1e16164b193b05a70576bab19bc2
Guilherme Komninakis <92609086+guikomninakis@users.noreply.github.com>
2023-05-12 19:59:36 -0300

    first commit
    first commit

---
+++ lib/theme/dark_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme(){
+return ThemeData(brightness: Brightness.dark,
+scaffoldBackgroundColor: Colors.grey[900],
+appBarTheme: AppBarTheme(color: Colors.grey[600]),
+);
+}
